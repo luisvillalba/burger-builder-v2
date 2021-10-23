e7949108a58f80aadb9cb07796d33ba1a0b2a68f
Luis Villalba <luismvillalba@gmail.com>
2021-10-23 00:25:12 -0700

    Cleaning up code and adding editorconfig

--- src/components/BurgerBuilderSummary/BurgerBuilderSummary.js
+++ src/components/BurgerBuilderSummary/BurgerBuilderSummary.js
@@ -4,14 +4,14 @@ import { Paper, Button } from '@material-ui/core';
 import css from './BurgerBuilderSummary.module.css';
 
 export default (props) => {
-    return (
-        <Paper className={css.Card}>
-            <h3>Total:</h3>
-            <p>{props.order.total}</p>
-            <Button
-                variant="contained"
-                color="primary"
-                onClick={props.onCheckout}>Checkout</Button>
-        </Paper>
-    ); 
+  return (
+    <Paper className={css.Card}>
+      <h3>Total:</h3>
+      <p>{props.order.total}</p>
+      <Button
+          variant="contained"
+          color="primary"
+          onClick={props.onCheckout}>Checkout</Button>
+    </Paper>
+  );
 }
