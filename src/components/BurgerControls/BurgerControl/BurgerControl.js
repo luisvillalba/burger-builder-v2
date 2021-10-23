387d33501f42744c1a675e69ae94797e1d9cee8f
Luis Villalba <luismvillalba@gmail.com>
2021-10-23 00:28:17 -0700

    Adding prettier to the codebase

--- src/components/BurgerControls/BurgerControl/BurgerControl.js
+++ src/components/BurgerControls/BurgerControl/BurgerControl.js
@@ -1,26 +1,25 @@
-
-import React from 'react';
-import { Box, IconButton, Grid } from '@material-ui/core';
-import AddCircle from '@material-ui/icons/AddCircle';
-import RemoveCircle from '@material-ui/icons/RemoveCircle';
+import React from "react";
+import { Box, IconButton, Grid } from "@material-ui/core";
+import AddCircle from "@material-ui/icons/AddCircle";
+import RemoveCircle from "@material-ui/icons/RemoveCircle";
 
 export default (props) => {
-    return (
-        <Box>
-            <Grid container>
-                <Grid item xs={6} alignContent="center">
-                    <p>{props.name}</p>
-                </Grid>
-                <Grid item xs={6} justify="flex-end">
-                    <IconButton onClick={props.removeIngredient}>
-                        <RemoveCircle />
-                    </IconButton>
-                    <span>{props.quantity}</span>
-                    <IconButton onClick={props.addIngredient}>
-                        <AddCircle />
-                    </IconButton>
-                </Grid>
-            </Grid>
-        </Box>
-    ); 
-}
+  return (
+    <Box>
+      <Grid container>
+        <Grid item xs={6} alignContent="center">
+          <p>{props.name}</p>
+        </Grid>
+        <Grid item xs={6} justify="flex-end">
+          <IconButton onClick={props.removeIngredient}>
+            <RemoveCircle />
+          </IconButton>
+          <span>{props.quantity}</span>
+          <IconButton onClick={props.addIngredient}>
+            <AddCircle />
+          </IconButton>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
--- src/components/BurguerRepresentation/BurgerRepresentation.js
+++ src/components/BurguerRepresentation/BurgerRepresentation.js
@@ -1,24 +1,26 @@
-
-import React from 'react';
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
-import { Box } from '@material-ui/core';
+import React from "react";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+import { Box } from "@material-ui/core";
 
 export default (props) => {
-    let ingredients = props.order.ingredients;
+  let ingredients = props.order.ingredients;
 
-    function getIngredients(name, quantity) {
-        let ingredients = []
-        for (let i = 0; i < quantity; i++) {
-            ingredients.push(<BurgerIngredient key={name + i} itype={name}/>);
-        }
-        return ingredients;
+  function getIngredients(name, quantity) {
+    let ingredients = [];
+    for (let i = 0; i < quantity; i++) {
+      ingredients.push(<BurgerIngredient key={name + i} itype={name} />);
     }
-    
-    return (
-        <Box minWidth={240} width="100%" maxWidth={350}>
-            <BurgerIngredient itype="breadTop" />
-                {Object.keys(ingredients).reduce((ac,ing) => ac.concat(getIngredients(ing, ingredients[ing])), [])}
-            <BurgerIngredient itype="breadBottom"/>
-        </Box>
-    );
-}
+    return ingredients;
+  }
+
+  return (
+    <Box minWidth={240} width="100%" maxWidth={350}>
+      <BurgerIngredient itype="breadTop" />
+      {Object.keys(ingredients).reduce(
+        (ac, ing) => ac.concat(getIngredients(ing, ingredients[ing])),
+        []
+      )}
+      <BurgerIngredient itype="breadBottom" />
+    </Box>
+  );
+};
